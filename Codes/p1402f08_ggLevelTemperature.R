a195e515fc4e3fd3c5b97a272209129e2fe4a551
tian <bwtian@gmail.com>
2014-11-20 16:21:55 +0900

    / ‘Codes/p1402f08_ggLevelTemperature.R’

--- Codes/p1402f08_ggLevelTemperature.R
+++ Codes/p1402f08_ggLevelTemperature.R
@@ -4,20 +4,19 @@ getwd()
 hkdKT  <- readRDS("hkd/hkd_kt3dlcc_140530_114352.Rds")
 head(hkdKT)
 #df  <- subset(hkdKT, Z %% 200 == 0)
-df  <- subset(hkdKT, Z == 100 | Z == 300 | Z == 500 | Z == 700 | Z == 900 | Z == 1000 |
+df  <- subset(hkdKT, Z == 100 | Z == 300 | Z == 500 | Z == 700 | Z == 900 |
                  Z == 1100 |  Z == 1200 | Z == 1300 | Z == 1400 ) 
 g1 <- ggplot(df) +
 geom_raster(aes(x = X, y =Y, fill = Temperatrue)) +
 facet_wrap(~Z)
-
 g2  <- g1 + scale_x_continuous(label = function(x) x/1000) +
 scale_y_continuous(label = function(x) x/1000) +
 xlab("Easting (km)") +
 ylab("Northing (km)")
 
 
-y  <- as.numeric(df$Temperatrue)
-hist(y)
+# y  <- as.numeric(df$Temperatrue)
+# max(y)
 #breaksY = c(0,100,200,250,300,350,400, max(y))
 breaksY = c(0,100,200,250,300,350,400, 515)
 labelsY = as.character(breaksY)
