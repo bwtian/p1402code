a2eb72837ccd2a436030c7d6ac75f95c0c91b152
tian <bwtian@gmail.com>
2014-11-26 10:57:02 +0900

    / ‘Codes/p1402f03_BoxplotByDepth.R’

--- Codes/p1402f03_BoxplotByDepth.R
+++ Codes/p1402f03_BoxplotByDepth.R
@@ -1,24 +1,18 @@
+# f03
 source("~/SparkleShare/Rprofile/R/Rsettings/phdRsettings.R")
 hkdBH  <- readRDS("~/Dropbox/2data/dataProduct/hkd/hkd_profiles_140806_164333.Rds")
 hkd100  <- subset(hkdBH, (Depths >=100 ))
 hkdxyz  <-  unique(hkdBH[,c(1:3,13)])
-#plot(hkd100$Depths, hkd100$Temperature, type = "p", pch = 20)
-bh  <- ggplot(data = hkd100, aes(x= Depths, y = Temperature)) +
-        geom_point(aes(color = Temperature), shape = ".") +
-        #geom_smooth(color = "blue") + 
-        stat_smooth(method = "lm", color = "red") +
-        annotate("text", x = 1100, y = 3, label = r2label, colour="red", 
-                 size = 4, parse=TRUE, font = "times") + 
-        xlab("Depth (m)") +
-        geom_hline(yintercept = 15, linetype = 2,color = "green") +
-        geom_hline(yintercept = 100, linetype = 3,color = "blue") +
-        scale_x_continuous(breaks = breaksX, labels = labelsX) +
-        scale_y_continuous(breaks = breaksY, labels = labelsY) +
-        ylab(expression(Temperature~(degree*C))) +
-        scale_colour_gradientn(name = expression(Temperature~(degree*C)), 
-                               colours = rev(rainbow(7)), 
-                               breaks = breaksY, 
-                               labels = format(breaksY)) +
-        theme_bw(base_size = 11, base_family = "Times")
-bh
-f03_f03box1 <-ggTlines
+boxp  <- ggplot(hkdxyz, aes(factor(0),TD)) +
+  geom_boxplot(fill = "green",outlier.colour = "red") +
+  #geom_boxplot(fill = "green", outlier.size = 0 ) +
+  geom_jitter(shape = ".", size  = 1) +
+  stat_summary(fun.y="mean",geom="point",color="blue", shape = 4) +
+  ylab("Total depth of each borehole (m)") + 
+  scale_y_continuous(breaks = c(seq(200, 2200,by = 200))) +
+  xlab("") + 
+  theme_bw(base_size = 12, base_family = "Times") +
+  theme(axis.text.y=element_blank(),axis.ticks.y=element_blank(), aspect.ratio = 1/8) +
+  coord_flip() 
+boxp
+# phd.ggsave(boxp)
