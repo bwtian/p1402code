99315c80aaab91ae0a61ea49f360678bf40c3f13
tian <bwtian@gmail.com>
2014-11-26 23:54:45 +0900

    / ‘Codes/p1402f02_ggHokkaidoGeology.R’

--- Codes/p1402f02_ggHokkaidoGeology.R
+++ Codes/p1402f02_ggHokkaidoGeology.R
@@ -57,21 +57,19 @@ cols <- c("accretionary complex" = "red",
 ggRock  <-  ggBase +  
   geom_polygon(aes(long,lat,group=group, fill=id), hkdRocks.df) + 
   scale_fill_manual(name =  "Rock types", values =cols)
-ggFault  <- ggBase +
-  geom_path(aes(long, lat, group=group, color = "Faults"), hkdFault.df,
+ggFault  <- ggRock +
+  geom_path(aes(long, lat, group=group, size = factor(0)), hkdFault.df,
             color = "black",
-            alpha = 0.7) +
-  scale_color_manual(name =  "", values = "black" labels = "Faults")
-ggFault
-scale_linetype_manual(name =  "Tectonic lines", values = c(1,3),
-                        labels = c("Tectonic lines","Volcanic front"))
-ggVol  <- ggBH  +
+            alpha = 0.7)  +
+  scale_size_manual(name =  "", values = 0.5 ,labels = "Faults")
+
+ggVol  <- ggFault  +
   geom_point(data = volA@data,
              aes(as.numeric(lon), as.numeric(lat),
                  color="red"),  shape = 17, size = 3)  +
-  scale_color_manual(name =  "Volcanoes", values = c("orange","red"), labels = c("Quaternary Volcanoes","Active Volcanoes")) +
+  scale_color_manual(name =  "Volcanoes", values = c("red"), labels = c("Active volcanoes")) +
 
-ggBar  <- ggSap +scaleBar(lon = 139, lat = 40, distanceLon = 50, distanceLegend = 30,distanceLat = 15, dist.unit = "km", arrow.length = 60, arrow.distance = 650, arrow.North.size = 4,legend.colour = "white", arrow.North.color = "white", arrow.colour = "blue")
+ggBar  <- ggVol +scaleBar(lon = 139, lat = 40, distanceLon = 50, distanceLegend = 30,distanceLat = 15, dist.unit = "km", arrow.length = 60, arrow.distance = 650, arrow.North.size = 4,legend.colour = "white", arrow.North.color = "white", arrow.colour = "blue")
 
 ggFont  <- ggBar +
   #coord_equal() +
@@ -79,27 +77,6 @@ ggFont  <- ggBar +
 # ggFont
 ##ge.ggsave(hkdStudyArea)
 
-## ggWRS2 path
-
-library(wrspathrow)
-wrs2.SPDF  <- pathrow_num(x = hkdLand, as_polys = TRUE)
-#plot(wrs2.SPDF,col = "red")
-#wrs2.SPDF@data
-wrs2.df  <- fortify(wrs2.SPDF)
-
-ggWRS  <- ggFont + geom_polygon(aes(long,lat,group=group),
-                                color = "grey", alpha = 0.3, fill = NA,
-                                linetype = 3,
-                                data=wrs2.df) +
-  geom_point(data = wrs2.SPDF@data, color = "black",
-             aes(x = centroid_x, y = centroid_y, shape = MODE ))
-
-ggWRS2  <- ggWRS +  geom_text(data = wrs2.SPDF@data,
-                              aes(x = centroid_x, y = centroid_y),
-                              label = paste(wrs2.SPDF@data$PATH, wrs2.SPDF@data$ROW," "),
-                              family="Times", face = "Italic", colour="black") +
-  scale_shape_manual(name =  " WRS 2", values = 20 , labels = c("Path and Row"))
-### Plate Boundaries
 
 jpArc.sldf  <- readRDS("~/Dropbox/2data/dataProduct/jp/jpPlateBoundary_141124_223221.Rds")
 #plot(jpArc.sldf)
@@ -130,9 +107,6 @@ ggPlate  <- ggWRS2 + geom_path(aes(long,lat,group=piece),
             hjust = -0.1, angle = 78, family="Times", colour="white",
             size = 4)
 
-
-
-ggPlate
 jpTlines.sldf  <- readRDS("~/Dropbox/2data/dataProduct/jp/jpTlines_141125_221917.Rds")
 hkdTlines.sldf  <- crop(jpTlines.sldf, bbox2.SPDF)
 plot(hkdTlines.sldf)
@@ -149,5 +123,5 @@ ggTlines  <- ggPlate + geom_line(aes(long,lat,group=group, linetype=factor(id2))
   scale_linetype_manual(name =  "Tectonic lines", values = c(1,3),
                         labels = c("Tectonic lines","Volcanic front")) 
 
-f01_hkdStudyArea  <-ggTlines
-#ge.ggsave(f01_hkdStudyArea)
+f02_hkdGeology  <-ggTlines
+ge.ggsave(f01_hkdStudyArea)
