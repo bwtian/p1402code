3932f6721b1437610f32f2e8ff2c26b70c1d3fd1
lab1 <bwtian@gmail.com>
2014-11-26 17:48:52 +0900

    + ‘Codes/downDEM.R’

---
+++ Codes/downDEM.R
@@ -0,0 +1,22 @@
+### hkdExtent
+
+xmin <- 139
+xmax <- 146
+ymin <- 41
+ymax <- 46
+bbox.SPDF <- ge.xy2bboxSPDF(xmin,xmax,ymin,ymax,wgs84GRS)
+hkdExtent  <- bbox.SPDF
+ge.sp2shpGeo(hkdExtent)
+getwd()
+
+
+library(raster)
+a  <- getData("SRTM", lat = 42.5, lon = 142.5)
+plot(a)
+getData("ISO3")
+japan  <- getData('alt', country='JPN')
+plot(japan)
+hkd  <- crop(japan, hkdExtent)
+plot(hkd)
+writeRaster(hkd, "hkdDEM.tif")
+contour(hkd,nlevels = )
