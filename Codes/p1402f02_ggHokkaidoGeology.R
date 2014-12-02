747e9bac0f6d4329afe20e33f53b8c6e8c050769
tian <bwtian@gmail.com>
2014-12-02 21:27:40 +0900

    / ‘Codes/p1402f02_ggHokkaidoGeology.R’

--- Codes/p1402f02_ggHokkaidoGeology.R
+++ Codes/p1402f02_ggHokkaidoGeology.R
@@ -139,14 +139,14 @@ ggBar  <- ggTlines  +
            arrow.distance = 680, arrow.North.size = 4,
            legend.colour = "white", arrow.North.color = "white", arrow.colour = "blue")
 ggFont  <- ggBar +
-  #coord_equal() +
-  theme_bw(base_family = "Times", base_size = 12)
+  coord_equal() +
+  theme_bw(base_family = "Times", base_size = 12) +
   theme(axis.text.y = element_text(angle = 90, hjust = 0.5, vjust = 0),
-              axis.title.x = element_text(vjust = -0.5))
-
+       axis.title.x = element_text(vjust = -0.5))
 # g  <- guide_legend("Tectonic lines")
 # ggGuid  <- ggFont + guides(size = g, linetype=g)
 hkdGeology  <-ggFont
+hkdGeology
 # 7*5
 ggsave(plot = hkdGeology, "hkdGeology.pdf", width =7)
 #ge.ggsave(hkdGeology)
