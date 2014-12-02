307b1db7d81adf96cda2ea9b7cb483d728cce3b2
tian <bwtian@gmail.com>
2014-12-02 19:54:42 +0900

    / ‘Codes/p1402f03_BoxplotBySingle.R’

--- Codes/p1402f03_BoxplotBySingle.R
+++ Codes/p1402f03_BoxplotBySingle.R
@@ -3,7 +3,8 @@ source("~/SparkleShare/Rprofile/R/Rsettings/phdRsettings.R")
 hkdBH  <- readRDS("~/Dropbox/2data/dataProduct/hkd/hkd_profiles_140806_164333.Rds")
 hkd100  <- subset(hkdBH, (Depths >=100 ))
 hkdxyz  <-  unique(hkdBH[,c(1:3,13)])
-boxp  <- ggplot(hkdxyz, aes(factor(0),TD)) +
+hkdBox1   <- 
+  ggplot(hkdxyz, aes(factor(0),TD)) +
   geom_boxplot(fill = "green",outlier.colour = "red") +
   #geom_boxplot(fill = "green", outlier.size = 0 ) +
   geom_jitter(shape = ".", size  = 1) +
@@ -17,7 +18,5 @@ boxp  <- ggplot(hkdxyz, aes(factor(0),TD)) +
         axis.title.x=element_text(vjust = 0),
         aspect.ratio = 1/8) +
   coord_flip() 
-hkdBox1  <- boxp
 ge.ggsave(hkdBox1)
-ggsave(plot =hkdBox1, "hkdBox1.pdf", width = 7)
-getwd()
+ggsave(plot =hkdBox1, "hkdBox1.pdf", width = 7, height = 1.5)
