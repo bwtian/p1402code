bddab6521799903f6540a87269facba21d77f392
lab1 <bwtian@gmail.com>
2014-12-04 15:28:29 +0900

    / ‘Codes/georUk3D.R’

---
+++ Codes/georUk3D.R
@@ -0,0 +1,5 @@
+source("~/SparkleShare/Rprofile/R/Rsettings/phdRsettings.R")
+hkdbhs  <- readRDS("~/Dropbox//2data//dataProduct//hkd/hkdbh_141201_110703.Rds")
+hkd3dgrid  <- readRDS("~/Dropbox/2data//dataProduct/hkd/hkd15hgrid_141201_111200.Rds")
+spdf  <- hkdbhs
+grid  <- hkd3dgrid
