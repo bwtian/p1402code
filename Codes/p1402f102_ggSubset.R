a06c67110998ba5e0a945f71e5ac008a60db681f
lab1 <bwtian@gmail.com>
2014-12-06 06:06:10 +0900

    / ‘Codes/p1402f102_ggSubset.R’

--- Codes/p1402f102_ggSubset.R
+++ Codes/p1402f102_ggSubset.R
@@ -79,8 +79,8 @@ gglst  <- function(df){
                                      name = lst.name) +
                 coord_equal() +
                 theme_bw(base_size = 12, base_family = "Times") +
-                theme(legend.position="left", legend.margin=unit(0,"lines"))  +
-                theme(plot.margin = unit(c(0,-1.5,0,-5), "lines"))
+                theme(legend.position="right", legend.margin=unit(0,"lines"))  +
+                theme(plot.margin = unit(c(0,-1.5,0,-2), "lines"))
 
 }
 lst.grobs  <- list()
