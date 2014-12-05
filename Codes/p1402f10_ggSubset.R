32c8dcaa10429e0e3d71b5c1b7bf7c4c597fa7a8
lab1 <bwtian@gmail.com>
2014-12-06 04:50:29 +0900

    / ‘Codes/p1402f10_ggSubset.R’

--- Codes/p1402f10_ggSubset.R
+++ Codes/p1402f10_ggSubset.R
@@ -71,9 +71,8 @@ names(lst.clip.l) <- c("A","B","C","D")
 class(lst.clip.l["A"])
 dimnames(lst.clip.l[[2]])
 count  <- 0
-lst.grobs  <- lapply(lst.clip.l, function(df) {
-        n  <- count + 1
-        ggplot(df) +
+lst.grobs  <- lapply(1:length(lst.clip.l), function(i) {
+                ggplot(lst.clip.l[i]) +
                         geom_raster(aes(x,y, fill = hkdL8B10CenterMos)) +
                         scale_x_continuous(labels = function(x) x/1000 -1200) +
                         scale_y_continuous(labels = function(x) x/1000 -1400) +
