3c2dc997e5ea6d77a4c59272e172027c5524fe58
lab1 <bwtian@gmail.com>
2014-12-06 05:00:57 +0900

    / ‘Codes/p1402f10_ggSubset.R’

--- Codes/p1402f10_ggSubset.R
+++ Codes/p1402f10_ggSubset.R
@@ -85,7 +85,7 @@ lst.grobs  <- lapply(lst.clip.l, function(i) {
                         coord_equal() +
                         theme_bw(base_size = 12, base_family = "Times") +
                         theme(plot.margin = unit(c(0,-0.5,0,0), "lines")) +
-                        geom_text(data=df, aes(label=paste("LST", 1:4)),
+                        geom_text(data=df, aes(label=paste("LST")),
                           x=-Inf, y=Inf, hjust=-0.4, vjust=2, col = "red",fontface = "bold")
 #                         annotate("text", x = -Inf, y = Inf, label ="LST",
 #                                  hjust=-0.4, vjust=2, col="black", cex=6,
