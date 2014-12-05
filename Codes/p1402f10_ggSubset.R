0c6e9365ab11ec6c721d41ffddcba681837b5d12
lab1 <bwtian@gmail.com>
2014-12-05 20:11:10 +0900

    / ‘Codes/p1402f10_ggSubset.R’

--- Codes/p1402f10_ggSubset.R
+++ Codes/p1402f10_ggSubset.R
@@ -81,7 +81,7 @@ grobs  <- lapply(clipper.l, function(d) {
                              name = expression(~(degree*C))) +
                 coord_equal() +
                 theme_bw(base_size = 10, base_family = "Times") +
-                theme(legend.position="bottom")
+                theme(legend.position="left")
         })
 
 
