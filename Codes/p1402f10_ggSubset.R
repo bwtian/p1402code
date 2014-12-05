6081e04bbdc662a015275b0f8e9c3a0b50995f93
lab1 <bwtian@gmail.com>
2014-12-06 02:55:47 +0900

    / ‘Codes/p1402f10_ggSubset.R’

--- Codes/p1402f10_ggSubset.R
+++ Codes/p1402f10_ggSubset.R
@@ -27,8 +27,8 @@ ge.raster2df  <- function(rst){
 # hkdSST1500  <- hkdKT[hkdKT$Z == 1500,]
 d  <- as.data.frame(rbind(c(41.91, 140.87),
                           c(42.23, 139.94),
-                          c(42.83, 141.31),
-                          c(43.48, 144.17)))
+                          c(42.81, 141.31),
+                          c(43.48, 144.16)))
 names(d)  <- c("lat", "lon")
 dlcc  <- ge.crsTransform(d, lon, lat, xlcc, ylcc, wgs84GRS,lccWgs84)
 rad  <- 2500
