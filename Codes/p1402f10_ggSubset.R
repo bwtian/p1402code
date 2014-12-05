ffe65d6905342cb377fb3631c707e7af11af6bdd
lab1 <bwtian@gmail.com>
2014-12-06 02:13:26 +0900

    / ‘Codes/p1402f10_ggSubset.R’

--- Codes/p1402f10_ggSubset.R
+++ Codes/p1402f10_ggSubset.R
@@ -14,14 +14,14 @@ ge.raster2df  <- function(rst){
 # lulc.rsp <- projectRaster(lulc.rst, lst.rst,method="ngb")
 # writeRaster(lulc.rsp, "lulc100.tif")
 
-hkdKT  <- readRDS("hkd_kt3dlcc_140530_114352.Rds")
-hkdKT$t <- 10^(hkdKT$KT)
-hkdXyzt  <- hkdKT[,c(1:3,9)]
-names(hkdXyzt)  <- c("x","y","z","t")
+# hkdKT  <- readRDS("hkd_kt3dlcc_140530_114352.Rds")
+# hkdKT$t <- 10^(hkdKT$KT)
+# hkdXyzt  <- hkdKT[,c(1:3,9)]
+# names(hkdXyzt)  <- c("x","y","z","t")
 
-lulc.df  <- ge.raster2df("lulc100.tif")
-lst.df  <- ge.raster2df("hkdL8B10CenterMos.tif")
-sst.df  <- hkdXyzt[hkdXyzt$z == 1500,]
+# lulc.df  <- ge.raster2df("lulc100.tif")
+# lst.df  <- ge.raster2df("hkdL8B10CenterMos.tif")
+# sst.df  <- hkdXyzt[hkdXyzt$z == 1500,]
 
 # summary(hkdKT)
 # hkdSST1500  <- hkdKT[hkdKT$Z == 1500,]
