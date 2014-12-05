1d3e6ff7af8111153e0a6c582db7c0e1f0fcfd89
lab1 <bwtian@gmail.com>
2014-12-05 20:15:51 +0900

    / ‘Codes/p1402f09_ggLST.R’

--- Codes/p1402f09_ggLST.R
+++ Codes/p1402f09_ggLST.R
@@ -137,10 +137,10 @@ p6  <- p3 + geom_rect(data = dlcc,
 # p3)
 p7  <- p6 +  coord_equal() +
         theme_bw(base_size = 12, base_family = "Times") +
-        theme(axis.title.x(vjust=0))
+        theme(axis.title.x =element_text(vjust=0))
 
 hkdLST  <- p7
-ggsave(plot=hkdLST, "hkdLST.pdf", width = 7, height = 5.5)
+ggsave(plot=hkdLST, "hkdLST2.pdf", width = 7, height = 5.5)
 #ge.ggsave(p7)
 # round(dlcc)
 # names(d)  <- c("lat","lon")
