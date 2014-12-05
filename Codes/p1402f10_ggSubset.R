01e586b9d8cd53ab96580a20f8925496ef5e784f
lab1 <bwtian@gmail.com>
2014-12-06 03:59:30 +0900

    / ‘Codes/p1402f10_ggSubset.R’

--- Codes/p1402f10_ggSubset.R
+++ Codes/p1402f10_ggSubset.R
@@ -131,7 +131,7 @@ lulc.grobs  <- lapply(lulc.clip.l, function(df) {
                 theme(legend.position="none")  +
                 theme(plot.margin = unit(c(0,-0.5,0,-0.5), "lines")) +
                 annotate("text", x = -Inf, y = Inf, label = "LULC",
-                         hjust=0, vjust=2, col="black", cex=6,
+                         hjust=1, vjust=2, col="black", cex=6,
                          fontface = "bold")
 })
 
