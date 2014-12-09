8f7a8d058c383c5ea211d2e39ec8b56f3e445732
lab1 <bwtian@gmail.com>
2014-12-09 12:36:26 +0900

    / ‘Codes/p1402f102_ggSubset.R’

--- Codes/p1402f102_ggSubset.R
+++ Codes/p1402f102_ggSubset.R
@@ -195,8 +195,8 @@ lulc.col  <-rbind(ggplotGrob(lulc.grobs[[1]]),
 grid.arrange(lst.col, lulc.col,ncol = 2,
 main = textGrob("LST                                                      LULC",
                 gp=gpar(font=2,fontfamily = "times")),
-left =  textGrob("A\n\n\n\n\n\nB\n\n\n\n\n\n\nC\n\n\n\n\n\n\nD", rot =0, vjust =0,
-gp=gpar(font=2,fontfamily = "times")))
+left =  textGrob("A\n\n\n\n\n\nB\n\n\n\n\n\n\nC\n\n\n\n\n\n\nD",
+                 gp=gpar(font=2,fontfamily = "times")))
 # dev.off()
 # getwd()
 #grid.arrange(lst.col,lulc.col,ncol = 2)
