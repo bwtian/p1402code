2c497f2c0f401d434b08a8eb65c1e112570edb29
lab1 <bwtian@gmail.com>
2014-12-10 18:25:16 +0900

    / ‘Codes/p1402f09_ggLST.R’

--- Codes/p1402f09_ggLST.R
+++ Codes/p1402f09_ggLST.R
@@ -45,10 +45,7 @@ p3  <- p2 + scale_fill_gradientn(colours = cols,
                                  na.value="white",
                           breaks = brks,
                           #name = expression(paste("Temperature from \nscene average", expression(~degree*C), sep = "\n"))
-                          name=parse(text=paste("Temperature from \nscene average\n", "^o ", "*C", sep=""))
-#p3
-
-#ge.ggsave(p3
+                          name=parse(text=paste("Temperature from \nscene average\n", "^o ", "*C", sep="")))
 ### North Arror and scale bar
 
 library(ggplot2)
