a64342dfbbdfb53a9fa0c20a460427bb1800aabf
tian <bwtian@gmail.com>
2014-12-14 23:50:29 +0900

    / ‘Codes/p1402f08_ggLevelTemperature.R’

--- Codes/p1402f08_ggLevelTemperature.R
+++ Codes/p1402f08_ggLevelTemperature.R
@@ -185,7 +185,7 @@ ggContour  <-     ggDensity  +
 
 HL_text <- data.frame(x = c(1300000,1500000, 1450000,1350000), y = c(1550000,1650000,1480000,1800000), lab = c("H", "H", "L", "L"),  ZZ = factor("Depth 1500 m", levels = c("Depth 100 m", "Depth 300 m", "Depth 500 m",  "Depth 700 m","Depth 900 m","Depth 1100 m", "Depth 1300 m", "Depth 1500 m")))
 ggHL  <- ggContour +
-        geom_text(data = HL_text, aes(x, y, label = lab))
+        geom_text(data = HL_text, aes(x, y, label = lab),alpha = 0.5)
 
 
 hkd3D  <- ggHL
@@ -193,6 +193,6 @@ hkd3D
 # library(directlabels)
 # direct.label(hkd3D)
 # ge.ggsave(hkd3D)
-#ggsave(plot = hkd3D, "hkd3D3.pdf", width =7, height = 9)
-# getwd()
+ggsave(plot = hkd3D, "hkd3D3.pdf", width =7, height = 9)
+getwd()
 
