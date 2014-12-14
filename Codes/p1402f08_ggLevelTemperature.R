851b095ecc37d0010c84d25146be6f4232f8705b
tian <bwtian@gmail.com>
2014-12-14 23:17:55 +0900

    / ‘Codes/p1402f08_ggLevelTemperature.R’

--- Codes/p1402f08_ggLevelTemperature.R
+++ Codes/p1402f08_ggLevelTemperature.R
@@ -133,7 +133,7 @@ hkdHeatflow.df$cut  <- cut(x = hkdHeatflow.df$Heat.Flow, breaks =breaksH)
 
 ggHeatflow  <-
         ggCirles +
-        geom_point(data =hkdHeatflow.df, aes(x, y, shape = cut), alpha =0.6, color = "grey") +
+        geom_point(data =hkdHeatflow.df, aes(x, y, shape = cut), alpha =1, color = "grey") +
         #geom_text(data =hkdHeatflow.df, aes(x, y,label=Heat.Flow), size=3, hjust=-.25, vjust=.75) +
         scale_shape_manual(name = expression("Heat flow (at depth 1100 m)"~(mW/m^2)),
                                                            values = c(1,16,2,17,0,15))
