b2df47fc9f1f8184daf8906e89d93dde9a4e62d6
tian <bwtian@gmail.com>
2015-01-28 16:34:53 +0900

    / ‘Codes/RSf06R1_BoxplotByDepth.R’

--- Codes/RSf06R1_BoxplotByDepth.R
+++ Codes/RSf06R1_BoxplotByDepth.R
@@ -33,7 +33,7 @@ hkdBox2  <- ggplot(hkdS, aes(x = cutDepth, y = Temperature)) +
   theme_bw(base_size = 12, base_family = "Times") +
   theme(axis.title.x = element_text(vjust = -0.5))
 hkdBox2 + coord_flip()
-#ggsave(plot =hkdBox2, "hkdBox2.pdf", width = 7, height = 7)
+ggsave(plot =hkdBox2, "hkdBox2.pdf", width = 7, height = 7)
 #ge.ggsave(f05box2)
 
 #
