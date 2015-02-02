6b94bf71bc4f4a1cf74cc4b4cc94ca6c65e9bb13
lab1 <bwtian@gmail.com>
2015-02-02 15:47:49 +0900

    / ‘Codes/RSf12R3.R’

--- Codes/RSf12R3.R
+++ Codes/RSf12R3.R
@@ -185,6 +185,7 @@ gglulc  <- function(df){
                                   name = lulc.name) +
                 coord_equal() +
                 theme_bw(base_size = 12, base_family = "Times") +
+                theme(legend.position="none")  +
                 theme(plot.margin = unit(c(1,-1.5,0,-1.5), "lines")) +
                 theme(axis.text.y = element_text(angle = 90, hjust = 0.5, vjust = 0),
                       axis.title.x = element_text(vjust = 0))
