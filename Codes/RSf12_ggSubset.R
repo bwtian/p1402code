668a6ef2e2b4bc8c84a51aefacbd865c1b062bc7
lab1 <bwtian@gmail.com>
2015-02-02 11:14:03 +0900

    / ‘Codes/RSf12_ggSubset.R’

--- Codes/RSf12_ggSubset.R
+++ Codes/RSf12_ggSubset.R
@@ -28,14 +28,14 @@ ge.raster2df  <- function(rst){
 # summary(sst.df)
 # summary(hkdKT)
 # hkdSST1500  <- hkdKT[hkdKT$Z == 1500,]
-# d  <- as.data.frame(rbind(c(41.91, 140.87),
-#                           c(42.23, 139.94),
-#                           c(42.88, 141.29),
-#                           c(43.47, 144.19)))
 d  <- as.data.frame(rbind(c(41.91, 140.87),
                           c(42.23, 139.94),
                           c(42.88, 141.29),
-                          c(43.45, 144.17)))
+                          c(43.47, 144.19)))
+# d  <- as.data.frame(rbind(c(41.91, 140.87),
+#                           c(42.23, 139.94),
+#                           c(42.88, 141.29),
+#                           c(43.45, 144.17)))
 names(d)  <- c("lat", "lon")
 dlcc  <- ge.crsTransform(d, lon, lat, xlcc, ylcc, wgs84GRS,lccWgs84)
 # dlcc  <- ge.crsTransform(d, lon, lat, xlcc, ylcc, wgs84GRS,lccWgs84)
