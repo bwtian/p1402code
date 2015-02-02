97c125ecafaa5db5f29ce287c2796cc34bedd6ec
lab1 <bwtian@gmail.com>
2015-02-02 11:37:41 +0900

    / ‘Codes/RSf12R1.R’

--- Codes/RSf12R1.R
+++ Codes/RSf12R1.R
@@ -34,7 +34,7 @@ ge.raster2df  <- function(rst){
 #                           c(43.47, 144.19)))
 d  <- as.data.frame(rbind(c(41.91, 140.87),
                           c(42.23, 139.94),
-                          c(42.83, 141.27),
+                          c(42.83, 141.31),
                           c(43.45, 144.158)))
 names(d)  <- c("lat", "lon")
 dlcc  <- ge.crsTransform(d, lon, lat, xlcc, ylcc, wgs84GRS,lccWgs84)
