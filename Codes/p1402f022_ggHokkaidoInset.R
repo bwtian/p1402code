0f091bb44ebf9583051108c596f6bf3027e9d1a4
tian <bwtian@gmail.com>
2015-02-05 12:15:27 +0900

    / ‘Codes/p1402f022_ggHokkaidoInset.R’

--- Codes/p1402f022_ggHokkaidoInset.R
+++ Codes/p1402f022_ggHokkaidoInset.R
@@ -68,7 +68,7 @@ ggVol  <- ggBH  +
                    aes(as.numeric(lon), as.numeric(lat),
                        color="red"),  shape = 17, size = 2)  +
         geom_point(data = volAA@data,
-                   aes(as.numeric(lon), as.numeric(lat),color="white",),
+                   aes(as.numeric(lon), as.numeric(lat),color="white"),
                    shape = 3, size = 2) +
         scale_color_manual(name =  "Volcanoes", values = c("orange","red", "green"), labels = c("Quaternary volcanoes","Active volcanoes", "Most active volcanoes")) +
         geom_path(data = volQ2@data, aes(as.numeric(lon), as.numeric(lat)),size = 12, alpha = 0.2, colour = "yellow",lineend = "round")
